5ab7b8705e5f63954ee02a926386599a98a29336
Marta <emeraldraven27@gmail.com>
2023-08-07 21:26:13 +0300

    feat: submit solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    n = String(Math.abs(n));
+    let result = "";
+    let number;
   
+    for (let i = 0; i < n.length; i++) {
+     result = n[i] + result;
+     number = Number(result);
+    }
+      return  number;
+     
 }
